075456eb7f4749e5e6083ba5e5bf53066ea40f92
Zach Horn <zach.horn1919@gmail.com>
2024-11-06 22:25:29 -0600

    final challenge changes and code

--- EmployeeSQL/ORM code.sql
+++ EmployeeSQL/ORM code.sql
@@ -1,5 +1,4 @@
 ﻿
-
 CREATE TABLE "Department" (
     "dept_no" VARCHAR(10)   NOT NULL,
     "dept_name" VARCHAR(20)   NOT NULL,
@@ -16,7 +15,6 @@ CREATE TABLE "Employee" (
     "last_name" VARCHAR(25)   NOT NULL,
     "sex" CHAR(1)   NOT NULL,
     "hire_date" DATE   NOT NULL,
-    "dept_no" VARCHAR(10)   NOT NULL,
     CONSTRAINT "pk_Employee" PRIMARY KEY (
         "emp_no"
      )
@@ -24,7 +22,10 @@ CREATE TABLE "Employee" (
 
 CREATE TABLE "Salary" (
     "emp_no" VARCHAR(10)   NOT NULL,
-    "salary" Integer   NOT NULL
+    "salary" INTEGER   NOT NULL,
+    CONSTRAINT "pk_Salary" PRIMARY KEY (
+        "emp_no"
+     )
 );
 
 CREATE TABLE "Title" (
@@ -37,19 +38,22 @@ CREATE TABLE "Title" (
 
 CREATE TABLE "DepartmentEmployees" (
     "dept_no" VARCHAR(10)   NOT NULL,
-    "emp_no" VARCHAR(10)   NOT NULL
+    "emp_no" VARCHAR(10)   NOT NULL,
+    CONSTRAINT "pk_DepartmentEmployees" PRIMARY KEY (
+        "dept_no","emp_no"
+     )
 );
 
 CREATE TABLE "DepartmentManagers" (
     "dept_no" VARCHAR(10)   NOT NULL,
-    "emp_no" VARCHAR(10)   NOT NULL
+    "emp_no" VARCHAR(10)   NOT NULL,
+    CONSTRAINT "pk_DepartmentManagers" PRIMARY KEY (
+        "dept_no","emp_no"
+     )
 );
 
 ALTER TABLE "Employee" ADD CONSTRAINT "fk_Employee_title_id" FOREIGN KEY("title_id")
 REFERENCES "Title" ("title_id");
-
-ALTER TABLE "Employee" ADD CONSTRAINT "fk_Employee_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "Department" ("dept_no");
 
 ALTER TABLE "Salary" ADD CONSTRAINT "fk_Salary_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "Employee" ("emp_no");
@@ -66,6 +70,92 @@ REFERENCES "Department" ("dept_no");
 ALTER TABLE "DepartmentManagers" ADD CONSTRAINT "fk_DepartmentManagers_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "Employee" ("emp_no");
 
-SELECT * FROM "Department"
+
+SELECT 
+    E.emp_no, 
+    E.last_name, 
+    E.first_name, 
+    E.sex, 
+    S.salary
+FROM 
+    "Employee" E
+JOIN 
+    "Salary" S ON E.emp_no = S.emp_no;
+	
+
+SELECT 
+	first_name, 
+	last_name, 
+	hire_date 
+FROM "Employee" 
+WHERE hire_date 
+BETWEEN '1986-01-01' AND '1986-12-31';
 
 
+SELECT 
+	D.dept_no, 
+	D.dept_name, 
+	M.emp_no,
+	E.last_name, 
+	E.first_name 
+FROM "Department" D 
+JOIN "DepartmentManagers" M 
+ON D.dept_no = M.dept_no 
+JOIN "Employee" E 
+ON M.emp_no = E.emp_no;
+
+
+SELECT 
+	DE.dept_no,
+	DE.emp_no,
+	E.last_name,
+	E.first_name,
+	D.dept_name
+FROM "DepartmentEmployees" DE 
+JOIN "Employee" E 
+ON DE.emp_no = E.emp_no 
+JOIN "Department" D 
+ON DE.dept_no = D.dept_no;
+
+
+SELECT 
+	first_name,
+	last_name,
+	sex 
+FROM "Employee" 
+WHERE first_name = 'Hercules' 
+AND last_name 
+LIKE 'B%';
+
+
+SELECT 
+	E.emp_no,
+	E.last_name,
+	E.first_name
+FROM "Employee" E 
+JOIN "DepartmentEmployees" DE 
+ON E.emp_no = DE.emp_no 
+JOIN "Department" D 
+ON DE.dept_no = D.dept_no 
+WHERE D.dept_name = 'Sales';
+
+
+SELECT 
+	E.emp_no,
+	E.last_name,
+	E.first_name,
+	D.dept_name 
+FROM "Employee" E 
+JOIN "DepartmentEmployees" DE 
+ON E.emp_no = DE.emp_no 
+JOIN "Department" D 
+ON DE.dept_no = D.dept_no 
+WHERE D.dept_name 
+IN ('Sales', 'Development');
+
+
+SELECT last_name,
+COUNT(*) AS frequency 
+FROM "Employee" 
+GROUP BY last_name 
+ORDER BY frequency DESC;
