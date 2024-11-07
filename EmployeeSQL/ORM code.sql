e32ec9a811b38ef3c382e195b5dcbf4ea9d18289
Zach's Mac <zachhorn@Zachss-MacBook-Air.local>
2024-11-06 18:17:23 -0600

    pushing changes

---
+++ EmployeeSQL/ORM code.sql
@@ -0,0 +1,71 @@
+﻿
+
+CREATE TABLE "Department" (
+    "dept_no" VARCHAR(10)   NOT NULL,
+    "dept_name" VARCHAR(20)   NOT NULL,
+    CONSTRAINT "pk_Department" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "Employee" (
+    "emp_no" VARCHAR(10)   NOT NULL,
+    "title_id" VARCHAR(20)   NOT NULL,
+    "birth_date" DATE   NOT NULL,
+    "first_name" VARCHAR(20)   NOT NULL,
+    "last_name" VARCHAR(25)   NOT NULL,
+    "sex" CHAR(1)   NOT NULL,
+    "hire_date" DATE   NOT NULL,
+    "dept_no" VARCHAR(10)   NOT NULL,
+    CONSTRAINT "pk_Employee" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "Salary" (
+    "emp_no" VARCHAR(10)   NOT NULL,
+    "salary" Integer   NOT NULL
+);
+
+CREATE TABLE "Title" (
+    "title_id" VARCHAR(10)   NOT NULL,
+    "title" VARCHAR(20)   NOT NULL,
+    CONSTRAINT "pk_Title" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+CREATE TABLE "DepartmentEmployees" (
+    "dept_no" VARCHAR(10)   NOT NULL,
+    "emp_no" VARCHAR(10)   NOT NULL
+);
+
+CREATE TABLE "DepartmentManagers" (
+    "dept_no" VARCHAR(10)   NOT NULL,
+    "emp_no" VARCHAR(10)   NOT NULL
+);
+
+ALTER TABLE "Employee" ADD CONSTRAINT "fk_Employee_title_id" FOREIGN KEY("title_id")
+REFERENCES "Title" ("title_id");
+
+ALTER TABLE "Employee" ADD CONSTRAINT "fk_Employee_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Department" ("dept_no");
+
+ALTER TABLE "Salary" ADD CONSTRAINT "fk_Salary_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employee" ("emp_no");
+
+ALTER TABLE "DepartmentEmployees" ADD CONSTRAINT "fk_DepartmentEmployees_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Department" ("dept_no");
+
+ALTER TABLE "DepartmentEmployees" ADD CONSTRAINT "fk_DepartmentEmployees_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employee" ("emp_no");
+
+ALTER TABLE "DepartmentManagers" ADD CONSTRAINT "fk_DepartmentManagers_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Department" ("dept_no");
+
+ALTER TABLE "DepartmentManagers" ADD CONSTRAINT "fk_DepartmentManagers_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employee" ("emp_no");
+
+SELECT * FROM "Department"
+
+
